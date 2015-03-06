b419e624d9097de73718e3db4e39065db3f8bd94
Soren Peterson <soren468@gmail.com>
2015-03-05 20:22:54 -0800

    Oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    user = User.find_or_create_by(provider: auth.provider, uid: auth.uid)
+    user.name = auth.info.name
+    user.email = auth.info.email
+
+    user.oauth_token = auth.credentials.token
+    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+
+    user.save!
+
+    user
+  end
 end
