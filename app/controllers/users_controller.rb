ea8ceded4a0072d39950cdcef4d51b432ac57608
Cordelia Peters & Ryan Urie <pair+cordeliapeters.ryan-urie@devbootcamp.com>
2015-03-06 19:19:54 -0800

    working ajax calls to update current users location and to update group markers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,21 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = []
+    group = Group.find(params[:group_id])
+    group.users.each do |user|
+      if user.id != current_user.id
+        @users << user
+      end
+    end
+      render json: @users, status: 200
   end
 
   def show
+    p params[:id]
+    @user = User.find(params[:id])
+    p params[:latitude]
+    @user.update_attributes(latitude: params[:latitude], longitude: params[:longitude])
   end
+
 end
--- db/migrate/20150307011253_add_lat_and_long_to_users.rb
+++ db/migrate/20150307011253_add_lat_and_long_to_users.rb
@@ -1,6 +1,6 @@
 class AddLatAndLongToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :latitude, :integer
-    add_column :users, :longitude, :integer
+    add_column :users, :latitude, :decimal, :precision => "15", :scale => "12"
+    add_column :users, :longitude, :decimal, :precision => "15", :scale => "12"
   end
 end
