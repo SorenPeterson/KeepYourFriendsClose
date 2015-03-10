5a0f561e8971104682e69880e433ad1b04eec2c7
ryan-urie <ryanurie@gmail.com>
2015-03-10 11:23:55 -0700

    Pulled in skeleton. Header for login.

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,9 @@ ActiveRecord::Schema.define(version: 20150309232840) do
   create_table "groups", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.string   "description"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.float    "lattitude"
     t.float    "latitude"
     t.float    "longitude"
   end
