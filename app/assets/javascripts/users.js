e22afab90cb2c7d14fb24139e240686ce47ca7e2
mevanoff24 <mevanoff24@gmail.com>
2015-03-11 12:21:49 -0700

    I don't understand

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -23,9 +23,9 @@ $("#texting-form").on("submit", function(event){
     }).done(function(response){
     console.log("success");
     if (response.allow_texting == true) {
-    $("#texting-form").replaceWith("<label>Disable Text Updates </label><input type='submit' value='Disable'>")
+    $("#texting-form input[type=submit]").val("Disable")
   } else {
-    $("#texting-form").replaceWith("<label>Enable Text Updates </label><input type='submit' value='Enable'>")
+    $("#texting-form input[type=submit]").val("Enable")
   }
   })
 })
