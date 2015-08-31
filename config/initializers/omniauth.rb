69becad666c657813f8810b5fa2a4b4622afcb76
Soren Peterson <soren468@gmail.com>
2015-08-30 22:12:55 -0500

    update scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FB_AUTH_CLIENT_ID'], ENV['FB_AUTH_CLIENT_SECRET'], scope: 'email, user_about_me, publish_actions, user_birthday, user_hometown, user_friends, read_friendlists, friends_location, publish_actions'
+  provider :facebook, ENV['FB_AUTH_CLIENT_ID'], ENV['FB_AUTH_CLIENT_SECRET'], scope: 'email, user_about_me, publish_actions, user_birthday, user_hometown, user_friends, read_custom_friendlists, publish_actions'
 end
 
 
